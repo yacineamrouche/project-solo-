0277ddab4d9855bc6dd73eddb8ff32b6ea076b55
yacineamrouche <yacineamrouchr03@gmail.com>
2023-05-09 09:28:21 +0200

    projet

--- app.js
+++ app.js
@@ -19,48 +19,64 @@ function calculateDiff(diffMilliseconds) {
 
 // function to calculate age
 function changeValuesDates(year, month, day) {
+  
   $("#yearValue").text(year);
 
   $("#monthValue").text(month);
   
   $("#dayValue").text(day);
   // retrieving the day
- var day = Number($("#day").val());
+}
+var day = Number($("#day").val());
   // retrieving the month
  var month = Number($("#month").val());
   // retrieving the year
  var year = Number($("#year").val());
+
+
 function calculateAge() {
- 
+ console.log("hi")
   // check if valid date
   var inputtedDate = new Date(year, month, day);
   var currentDate = new Date();
   // check if valid date
   if (inputtedDate.getTime() < currentDate.getTime()) {
    var diffMilliseconds = currentDate - inputtedDate;
-    calculateDiff(diffMilliseconds) = [calculedYears, calculedMonths, calculedDays] ;
+    [calculedYears, calculedMonths, calculedDays]= calculateDiff(diffMilliseconds) 
     changeValuesDates(calculedYears, calculedMonths, calculedDays);
   }else{
     changeValuesDates("--","--","--");
   }
-}}
-$("#calculate").click(calculateAge);
-
-var x= new Date()
-var s=x.getMonth()
-var z=x.getDate()
-
-function nextBirthday(){
-  var t=0
-  if(month < s){
-     t= (month+ 12) - s
-  }
-  else if( month >=s){
-    t= month - s
-  }
-  nextBirthday(t)
 }
 
-$("Your Next Birthday").click(nextBirthday);
+$("#calculate").click(calculateAge);
+
+var x=new Date()
+var z=x.getMonth()
+var t= x.getDate()
+
+function nextBirth(){
+  var yy=0
+  var rr=0
+  var tt=0
+  if(month<z){
+    yy=((((month+11)) -z)*30)+day
+     rr=Math.floor(yy/30)
+     tt=yy%30
+     
+  }
+  else if(month>=z){
+    yy=((( month-z)-1)*30 )+day
+    rr=Math.floor(yy/30)
+    tt=yy%30
+    console.log(rr,tt,yy)
+     
+   }
+  changeValuesDates("--",rr,tt )
+}
+
+
+
+$("#change").click(nextBirth)
 
 
